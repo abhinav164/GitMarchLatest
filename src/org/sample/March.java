61bc0c421abbeb79adf1ff5a8378ae05f254c2d2
abhinav164 <supriya.lekkalla@gmail.com>
2022-03-31 19:28:23 +0530

    update my first project

---
+++ src/org/sample/March.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class March {
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println("number is:"+a);
+		System.out.println("code moved to remote machine");
+	}
+
+}
